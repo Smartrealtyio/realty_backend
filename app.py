89a3e5dea2a595402448bdb923ac527cda65ee8c
Egor14 <egorgrekhnev@mail.ru>
2019-11-14 18:01:21 +0300

    fix get request

--- app.py
+++ app.py
@@ -11,12 +11,30 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/api/mean/', methods=['POST'])
+@app.route('/api/mean/', methods=['GET'])
 def mean():
-    data = json.loads(request.data)
-    print(data)
+    # data = json.loads(request.data)
+    # print(data)
+    full_sq_from = float(request.args.get('full_sq_from'))
+    full_sq_to = float(request.args.get('full_sq_to'))
+    latitude_from = float(request.args.get('latitude_from'))
+    latitude_to = float(request.args.get('latitude_to'))
+    longitude_from = float(request.args.get('longitude_from'))
+    longitude_to = float(request.args.get('longitude_to'))
+    rooms = float(request.args.get('rooms'))
+    price_from = float(request.args.get('price_from')) if request.args.get('price_from') is not None else None
+    price_to = float(request.args.get('price_to')) if request.args.get('price_to') is not None else None
+    building_type_str = float(request.args.get('building_type_str')) if request.args.get('building_type_str') is not None else None
+    kitchen_sq = float(request.args.get('kitchen_sq')) if request.args.get('kitchen_sq') is not None else None
+    life_sq = float(request.args.get('life_sq')) if request.args.get('life_sq') is not None else None
+    renovation = float(request.args.get('renovation')) if request.args.get('renovation') is not None else None
+    has_elevator = float(request.args.get('has_elevator')) if request.args.get('has_elevator') is not None else None
+    floor_first = float(request.args.get('floor_first')) if request.args.get('floor_first') is not None else None
+    floor_last = float(request.args.get('floor_last')) if request.args.get('floor_last') is not None else None
 
-    mean_price, flats = MeanPrice.MeanPrices(**data)
+    mean_price, flats = MeanPrice.MeanPrices(full_sq_from, full_sq_to, rooms, latitude_from, latitude_to,
+                                             longitude_from, longitude_to, price_from, price_to, building_type_str,
+                                             kitchen_sq, life_sq, renovation, has_elevator, floor_first, floor_last)
 
     if math.isnan(mean_price):
         mean_price = None
@@ -25,7 +43,7 @@ def mean():
 
 @app.route('/predict/', methods=['GET', 'POST'])
 def predict():
-    return jsonify({'key':5})
+    return jsonify({'key': 5})
 
 
 if __name__ == '__main__':

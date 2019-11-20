4724ad8bc6532a0ec6c1e9aeac4065cec0ac5ac1
Egor14 <egorgrekhnev@mail.ru>
2019-11-20 16:28:31 +0300

    new syntax for old python

--- app.py
+++ app.py
@@ -126,180 +126,178 @@ def map():
     # return 'Price {0} \n Estimated Sale Time: {1} days'.format(price, term)
 
 
-# @app.route('/api/save/', methods=['POST'])
-# def save():
-#     flat = json.loads(request.data)
-#     try:
-#         conn = psycopg2.connect(host=SETTINGS.host, dbname=SETTINGS.name, user=SETTINGS.user, password=SETTINGS.password)
-#         cur = conn.cursor()
-#     except:
-#         return jsonify({'result': False})
-#
-#     cur.execute("select id from districts where name=%s;", (flat['district'],))
-#     try:
-#         district_id = cur.fetchone()[0]
-#     except:
-#         # logging.info('district does not exist')
-#         conn.close()
-#         return jsonify({'result': False})
-#     # logging.info('district_id' + str(district_id))
-#
-#     metro_ids = {}
-#     for metro in flat['metros']:
-#         try:
-#             cur.execute("select id from metros where name=%s;", (metro,))
-#             metro_id = cur.fetchone()[0]
-#             metro_ids.update({metro: metro_id})
-#         except:
-#             # logging.info('metro' + str(metro) + 'does not exist')
-#             # try:
-#             #     metro_location = 'Москва,метро '+ metro
-#             #     coords_response = requests.get(
-#             #         f'https://geocode-maps.yandex.ru/1.x/?apikey={self.yand_api_token}&format=json&geocode={metro_location}', timeout=5).text
-#             #     coords = \
-#             #     json.loads(coords_response)['response']['GeoObjectCollection']['featureMember'][0]['GeoObject'][
-#             #         'Point']['pos']
-#             #     longitude, latitude = coords.split(' ')
-#             #     longitude = float(longitude)
-#             #     latitude = float(latitude)
-#             #
-#             #     cur.execute("""insert into metros (longitude, latitude, city_id, created_at, updated_at, metro_id, name)
-#             #                    values (%s, %s, %s, %s, %s, %s, %s)""", (
-#             #         longitude,
-#             #         latitude,
-#             #         1,
-#             #         datetime.now(),
-#             #         datetime.now(),
-#             #         0,
-#             #         metro
-#             #     ))
-#             #     print('udated', metro)
-#             # except:
-#             # logging.info('fail in updating' + str(metro))
-#             continue
-#
-#     try:
-#         coords_response = requests.get(
-#             f'https://geocode-maps.yandex.ru/1.x/?apikey={SETTINGS.yand_api_token}&format=json&geocode={flat["address"]}',
-#             timeout=5).text
-#         coords = \
-#             json.loads(coords_response)['response']['GeoObjectCollection']['featureMember'][0]['GeoObject'][
-#                 'Point'][
-#                 'pos']
-#         longitude, latitude = coords.split(' ')
-#         longitude = float(longitude)
-#         latitude = float(latitude)
-#     except IndexError:
-#         # logging.info('bad address for yandex-api' + flat['address'])
-#         conn.close()
-#         return jsonify({'result': False})
-#
-#     cur.execute("select id from buildings where address=%s or longitude=%s and latitude=%s;",
-#                 (flat['address'], longitude, latitude))
-#     is_building_exist = cur.fetchone()
-#     if not is_building_exist:
-#
-#         cur.execute(
-#             """insert into buildings
-#                (max_floor, building_type_str, built_year, flats_count, address, renovation,
-#                 has_elevator, longitude, latitude, district_id, created_at, updated_at)
-#                values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""", (
-#                 flat['max_floor'],
-#                 flat['building_type_str'],
-#                 flat['built_year'],
-#                 flat['flats_count'],
-#                 flat['address'],
-#                 flat['renovation'],
-#                 flat['has_elevator'],
-#                 longitude,
-#                 latitude,
-#                 district_id,
-#                 datetime.now(),
-#                 datetime.now()
-#             ))
-#         cur.execute("select id from buildings where address=%s;", (flat['address'],))
-#         building_id = cur.fetchone()[0]
-#         # logging.info('building_id' + str(building_id))
-#         for metro, metro_id in metro_ids.items():
-#             try:
-#                 cur.execute(
-#                     """insert into time_metro_buildings (building_id, metro_id, time_to_metro, transport_type, created_at, updated_at)
-#                        values (%s, %s, %s, %s, %s, %s);""", (
-#                         building_id,
-#                         metro_id,
-#                         flat['metros'][metro]['time_to_metro'],
-#                         flat['metros'][metro]['transport_type'],
-#                         datetime.now(),
-#                         datetime.now()
-#                     ))
-#             except:
-#                 # logging.info('some new error')
-#                 conn.close()
-#                 return jsonify({'result': False})
-#     else:
-#         building_id = is_building_exist[0]
-#         # logging.info('building already exist' + str(building_id))
-#
-#     cur.execute('select * from flats where offer_id=%s', (flat['offer_id'],))
-#     is_offer_exist = cur.fetchone()
-#     if not is_offer_exist:
-#         cur.execute(
-#             """insert into flats (full_sq, kitchen_sq, life_sq, floor, is_apartment, building_id, created_at, updated_at, offer_id, closed, rooms_total, image)
-#                values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (
-#                 flat['full_sq'],
-#                 flat['kitchen_sq'],
-#                 flat['life_sq'],
-#                 flat['floor'],
-#                 flat['is_apartment'],
-#                 building_id,
-#                 datetime.now(),
-#                 datetime.now(),
-#                 'cian' + flat['offer_id'],
-#                 flat['closed'],
-#                 flat['rooms_count'],
-#                 flat['image']
-#             ))
-#         cur.execute('select id from flats where offer_id=%s;', (flat['offer_id'],))
-#         flat_id = cur.fetchone()[0]
-#         # logging.info('flat_id' + str(flat_id))
-#     else:
-#         flat_id = is_offer_exist[0]
-#         # logging.info('flat already exist' + str(flat_id))
-#
-#         cur.execute("""update flats
-#                        set full_sq=%s, kitchen_sq=%s, life_sq=%s, floor=%s, is_apartment=%s, building_id=%s, updated_at=%s, closed=%s, rooms_total=%s, image=%s
-#                        where id=%s""", (
-#             flat['full_sq'],
-#             flat['kitchen_sq'],
-#             flat['life_sq'],
-#             flat['floor'],
-#             flat['is_apartment'],
-#             building_id,
-#             datetime.now(),
-#             flat['closed'],
-#             flat['rooms_count'],
-#             flat['image'],
-#             flat_id
-#         ))
-#         # logging.info('updated' + str(flat_id))
-#
-#     for price_info in flat['prices']:
-#         cur.execute('select * from prices where changed_date=%s', (price_info[0],))
-#         is_price_exist = cur.fetchone()
-#         if not is_price_exist:
-#             cur.execute("""insert into prices (price, changed_date, flat_id, created_at, updated_at)
-#                            values (%s, %s, %s, %s, %s);""", (
-#                 price_info[1],
-#                 price_info[0],
-#                 flat_id,
-#                 datetime.now(),
-#                 datetime.now()
-#             ))
-#
-#     conn.commit()
-#     cur.close()
-#
-#     return jsonify({'result': True})
+@app.route('/api/save/', methods=['POST'])
+def save():
+    flat = json.loads(request.data)
+    try:
+        conn = psycopg2.connect(host=SETTINGS.host, dbname=SETTINGS.name, user=SETTINGS.user, password=SETTINGS.password)
+        cur = conn.cursor()
+    except:
+        return jsonify({'result': False})
+
+    cur.execute("select id from districts where name=%s;", (flat['district'],))
+    try:
+        district_id = cur.fetchone()[0]
+    except:
+        # logging.info('district does not exist')
+        conn.close()
+        return jsonify({'result': False})
+    # logging.info('district_id' + str(district_id))
+
+    metro_ids = {}
+    for metro in flat['metros']:
+        try:
+            cur.execute("select id from metros where name=%s;", (metro,))
+            metro_id = cur.fetchone()[0]
+            metro_ids.update({metro: metro_id})
+        except:
+            # logging.info('metro' + str(metro) + 'does not exist')
+            # try:
+            #     metro_location = 'Москва,метро '+ metro
+            #     coords_response = requests.get(
+            #         f'https://geocode-maps.yandex.ru/1.x/?apikey={self.yand_api_token}&format=json&geocode={metro_location}', timeout=5).text
+            #     coords = \
+            #     json.loads(coords_response)['response']['GeoObjectCollection']['featureMember'][0]['GeoObject'][
+            #         'Point']['pos']
+            #     longitude, latitude = coords.split(' ')
+            #     longitude = float(longitude)
+            #     latitude = float(latitude)
+            #
+            #     cur.execute("""insert into metros (longitude, latitude, city_id, created_at, updated_at, metro_id, name)
+            #                    values (%s, %s, %s, %s, %s, %s, %s)""", (
+            #         longitude,
+            #         latitude,
+            #         1,
+            #         datetime.now(),
+            #         datetime.now(),
+            #         0,
+            #         metro
+            #     ))
+            #     print('udated', metro)
+            # except:
+            # logging.info('fail in updating' + str(metro))
+            continue
+
+    try:
+        coords_response = requests.get('https://geocode-maps.yandex.ru/1.x/?apikey={}&format=json&geocode={}'.format(SETTINGS.yand_api_token, flat["address"]),timeout=5).text
+        coords = \
+            json.loads(coords_response)['response']['GeoObjectCollection']['featureMember'][0]['GeoObject'][
+                'Point'][
+                'pos']
+        longitude, latitude = coords.split(' ')
+        longitude = float(longitude)
+        latitude = float(latitude)
+    except IndexError:
+        # logging.info('bad address for yandex-api' + flat['address'])
+        conn.close()
+        return jsonify({'result': False})
+
+    cur.execute("select id from buildings where address=%s or longitude=%s and latitude=%s;",
+                (flat['address'], longitude, latitude))
+    is_building_exist = cur.fetchone()
+    if not is_building_exist:
+
+        cur.execute(
+            """insert into buildings
+               (max_floor, building_type_str, built_year, flats_count, address, renovation,
+                has_elevator, longitude, latitude, district_id, created_at, updated_at)
+               values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""", (
+                flat['max_floor'],
+                flat['building_type_str'],
+                flat['built_year'],
+                flat['flats_count'],
+                flat['address'],
+                flat['renovation'],
+                flat['has_elevator'],
+                longitude,
+                latitude,
+                district_id,
+                datetime.now(),
+                datetime.now()
+            ))
+        cur.execute("select id from buildings where address=%s;", (flat['address'],))
+        building_id = cur.fetchone()[0]
+        # logging.info('building_id' + str(building_id))
+        for metro, metro_id in metro_ids.items():
+            try:
+                cur.execute(
+                    """insert into time_metro_buildings (building_id, metro_id, time_to_metro, transport_type, created_at, updated_at)
+                       values (%s, %s, %s, %s, %s, %s);""", (
+                        building_id,
+                        metro_id,
+                        flat['metros'][metro]['time_to_metro'],
+                        flat['metros'][metro]['transport_type'],
+                        datetime.now(),
+                        datetime.now()
+                    ))
+            except:
+                # logging.info('some new error')
+                conn.close()
+                return jsonify({'result': False})
+    else:
+        building_id = is_building_exist[0]
+        # logging.info('building already exist' + str(building_id))
+
+    cur.execute('select * from flats where offer_id=%s', (flat['offer_id'],))
+    is_offer_exist = cur.fetchone()
+    if not is_offer_exist:
+        cur.execute(
+            """insert into flats (full_sq, kitchen_sq, life_sq, floor, is_apartment, building_id, created_at, updated_at, offer_id, closed, rooms_total, image)
+               values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (
+                flat['full_sq'],
+                flat['kitchen_sq'],
+                flat['life_sq'],
+                flat['floor'],
+                flat['is_apartment'],
+                building_id,
+                datetime.now(),
+                datetime.now(),
+                'cian' + flat['offer_id'],
+                flat['closed'],
+                flat['rooms_count'],
+                flat['image']
+            ))
+        cur.execute('select id from flats where offer_id=%s;', (flat['offer_id'],))
+        flat_id = cur.fetchone()[0]
+        # logging.info('flat_id' + str(flat_id))
+    else:
+        flat_id = is_offer_exist[0]
+        # logging.info('flat already exist' + str(flat_id))
+
+        cur.execute("""update flats
+                       set full_sq=%s, kitchen_sq=%s, life_sq=%s, floor=%s, is_apartment=%s, building_id=%s, updated_at=%s, closed=%s, rooms_total=%s, image=%s
+                       where id=%s""", (
+            flat['full_sq'],
+            flat['kitchen_sq'],
+            flat['life_sq'],
+            flat['floor'],
+            flat['is_apartment'],
+            building_id,
+            datetime.now(),
+            flat['closed'],
+            flat['rooms_count'],
+            flat['image'],
+            flat_id
+        ))
+        # logging.info('updated' + str(flat_id))
+
+    for price_info in flat['prices']:
+        cur.execute('select * from prices where changed_date=%s', (price_info[0],))
+        is_price_exist = cur.fetchone()
+        if not is_price_exist:
+            cur.execute("""insert into prices (price, changed_date, flat_id, created_at, updated_at)
+                           values (%s, %s, %s, %s, %s);""", (
+                price_info[1],
+                price_info[0],
+                flat_id,
+                datetime.now(),
+                datetime.now()
+            ))
+
+    conn.commit()
+    cur.close()
+
+    return jsonify({'result': True})
 
 
 

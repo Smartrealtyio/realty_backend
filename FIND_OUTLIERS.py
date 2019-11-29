9c2acf0bff4d14edbeee5138d1aecbdbec2ad2a3
StefanPushkov <stefan.pushkov@icloud.com>
2019-11-29 16:17:08 +0300

    Added option to filter received outliers data

--- FIND_OUTLIERS.py
+++ FIND_OUTLIERS.py
@@ -9,7 +9,8 @@ DATA_OUTLIERS = 'C:/Storage/DDG/DATA/PREPARED/COORDINATES_OUTLIERS.csv'
 MODEL_OUTLIERS = 'C:/Storage/DDG/MedianPrices/Server/outliers_models/models.joblib'
 
 
-def OutliersSearch():
+def OutliersSearch(full_sq: float, rooms: int, latitude_from: float, latitude_to: float,
+               longitude_from: float, longitude_to: float):
 
     data = pd.read_csv(DATA_OUTLIERS)
     data = data[['price_meter_sq', 'full_sq']]
@@ -32,6 +33,11 @@ def OutliersSearch():
     new_data['flat_id'] = new_data.index
     ds: pd.DataFrame = pd.merge(new_data, outliers_it, left_on="flat_id", right_on="flat_id", suffixes=['', 'double'])
     ds = ds.drop(['flat_id', 'full_sqdouble', 'price_meter_sqdouble'], axis=1)
+
+    filter = (((ds.full_sq >= full_sq-5) & (ds.full_sq <= full_sq+5)) & (ds.rooms == rooms) &
+              ((ds.latitude >= latitude_from)&(ds.latitude <= latitude_to)
+               &(ds.longitude >= longitude_from)&(ds.longitude <= longitude_to)))
+    ds = ds[filter]
 
 
     return "Outliers: \n\n {0}".format(ds.to_dict('index'))

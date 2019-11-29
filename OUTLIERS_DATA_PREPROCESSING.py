9f5fe9b4a7f703c4643f72da40408ff8545d9d55
Egor14 <egorgrekhnev@mail.ru>
2019-11-29 17:24:11 +0300

    new structure for model learning

--- OUTLIERS_DATA_PREPROCESSING.py
+++ OUTLIERS_DATA_PREPROCESSING.py
@@ -115,7 +115,7 @@ def main_preprocessing():
                      row['full_sq']), axis=1)
 
     ds = ds.drop(['max_floor', "flat_id", 'floor','building_type_str', 'life_sq', 'updated_at', 'changed_date',
-                  'id_building', 'district_id', 'transport_type'], axis=1)
+                 'district_id', 'transport_type'], axis=1)
     # ds = ds.drop(['kitchen_sq', 'X', 'longitude', 'has_elevator', 'latitude', 'Y', 'time_to_metro',
     #               'floor_first', 'renovation', 'floor_last', "is_apartment"], axis=1)
     print('HEADERS NAME FINALY: ', list(ds.columns))

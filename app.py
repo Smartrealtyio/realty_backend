7e4b83facced4ec2a388c23a5f9339d535e5228d
StefanPushkov <stefan.pushkov@icloud.com>
2019-12-03 18:58:02 +0300

    Add filter for price

--- app.py
+++ app.py
@@ -276,8 +276,8 @@ def map():
     print("Clean data: ", clean_data.shape)
 
     filter1 = (((clean_data.full_sq <= full_sq + 3) & (clean_data.full_sq >= full_sq - 3)) & (
-            (clean_data.longitude >= longitude - 0.01) & (clean_data.longitude <= longitude + 0.01) &
-            (clean_data.latitude >= latitude - 0.01) & (clean_data.latitude <= latitude + 0.01)) &
+            (clean_data.longitude >= longitude - 0.1) & (clean_data.longitude <= longitude + 0.1) &
+            (clean_data.latitude >= latitude - 0.1) & (clean_data.latitude <= latitude + 0.1)) &
                ((clean_data.price_meter_sq <= price_meter_sq + 20000) & (
                            clean_data.price_meter_sq >= price_meter_sq - 20000)) & (
                        (clean_data.time_to_metro >= time_to_metro - 2) & (
@@ -311,7 +311,7 @@ def map():
     filter1 = (((clean_data.full_sq <= full_sq + 3) & (clean_data.full_sq >= full_sq - 3)) & (
             (clean_data.longitude >= longitude - 0.1) & (clean_data.longitude <= longitude + 0.1) &
             (clean_data.latitude >= latitude - 0.1) & (clean_data.latitude <= latitude + 0.1)) &
-               
+
                (clean_data.term < 500) & ((clean_data.time_to_metro >= time_to_metro - 2) & (
                         clean_data.time_to_metro <= time_to_metro + 2)))
     ds = clean_data[filter1]

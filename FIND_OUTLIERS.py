aebd3d7b1f85fa9f570acf60416bfe54d6f64555
StefanPushkov <stefan.pushkov@icloud.com>
2019-12-06 13:19:50 +0300

    Edit typo

--- FIND_OUTLIERS.py
+++ FIND_OUTLIERS.py
@@ -5,8 +5,10 @@ import pandas as pd
 # import seaborn as sns
 from joblib import dump, load
 import settings_local as SETTINGS
+from sklearn.ensemble import GradientBoostingRegressor
 
 DATA_OUTLIERS = SETTINGS.DATA + '/COORDINATES_OUTLIERS.csv'
+PATH_TO_PRICE_MODEL = SETTINGS.MODEL + '/PriceModel.joblib'
 MODEL_OUTLIERS = SETTINGS.MODEL + '/models.joblib'
 
 
@@ -17,12 +19,15 @@ def OutliersSearch():
     # data = data[data.price_meter_sq < data.price_meter_sq.quantile(0.2)]
     print("Data price_meter_sq < price_meter_sq.quantile(0.2): ", data.shape, flush=True)
 
+    X1 = data[['renovation', 'has_elevator', 'longitude', 'latitude', 'full_sq', 'kitchen_sq',
+                 'is_apartment', 'time_to_metro', 'floor_last', 'floor_first', 'X', 'Y']]
+    data["price"] = np.log1p(data["price"])
+    y1 = data[['price']].values.ravel()
+    print(X1.shape, y1.shape)
 
-
-    clf = IsolationForest(contimation=0.2)
-    clf.fit(data)
-    # save model
-    dump(clf, MODEL_OUTLIERS)
+    clf = GradientBoostingRegressor(n_estimators=350, max_depth=4, verbose=10)
+    clf.fit(X1, y1)
+    dump(clf, PATH_TO_PRICE_MODEL)
 
 
 if __name__ == '__main__':
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import FIND_OUTLIERS
 from sklearn.cluster import KMeans
 import psycopg2
 import settings_local as SETTINGS
+PATH_TO_PRICE_MODEL = SETTINGS.MODEL + '/PriceModel.joblib'
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import IsolationForest
 from sklearn.ensemble import GradientBoostingRegressor
@@ -120,6 +121,7 @@ def mean():
         new_df = new_df[new_df.price <= price_to]
 
     # PRICE
+    '''
     X1 = new_df[['renovation', 'has_elevator', 'longitude', 'latitude', 'full_sq', 'kitchen_sq',
                  'is_apartment', 'time_to_metro', 'floor_last', 'floor_first', 'X', 'Y']]
     new_df["price"] = np.log1p(new_df["price"])
@@ -128,7 +130,8 @@ def mean():
 
     clf = GradientBoostingRegressor(n_estimators=350, max_depth=4, verbose=10)
     clf.fit(X1, y1)
-
+    '''
+    clf = load(PATH_TO_PRICE_MODEL)
     # new_df["price"] = np.expm1(new_df["price"])
     new_df['pred_price'] = new_df[['renovation', 'has_elevator', 'longitude', 'latitude', 'full_sq', 'kitchen_sq',
                                    'is_apartment', 'time_to_metro', 'floor_last', 'floor_first', 'X', 'Y']].apply(

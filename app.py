d6f725fe2aad592651ad5a36d67f3a8deb6729e5
StefanPushkov <stefan.pushkov@icloud.com>
2019-12-12 16:42:27 +0300

    Reduced the number of parameters to predict the term of sale

--- app.py
+++ app.py
@@ -330,7 +330,7 @@ def map():
     '''
 
     # GBR
-    GBR_TERM = GradientBoostingRegressor(n_estimators=150, max_depth=7, verbose=5, max_features=2, random_state=42)
+    GBR_TERM = GradientBoostingRegressor(n_estimators=150, max_depth=3, verbose=5, max_features=3, random_state=42)
     print(X_term.shape, y_term.shape)
     GBR_TERM.fit(X_term, y_term)
     term_gbr_pred = GBR_TERM.predict([[longitude, latitude, float(price), full_sq, X, Y]])

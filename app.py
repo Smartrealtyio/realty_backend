f1c81c5a36d06ee31f3431abc8bd6ffe9a764334
StefanPushkov <stefan.pushkov@icloud.com>
2020-01-17 00:09:36 +0300

    USe both: Yandex And Cian, fixing read flats.csv

--- app.py
+++ app.py
@@ -538,11 +538,11 @@ def map():
     b = {'x': int(term), 'y': int(price)}
     print("b: ", b, flush=True)
 
-    for i, item in enumerate(new_a):
-        if new_a[i[0]].get('y') < b.get('y') < new_a[i[0] + 1].get('y'):
-            b['x'] = int((new_a[i[0]].get('x') + new_a[i[0] + 1].get('x')) / 2)
-            print(new_a[i[0]], new_a[i[0] + 1])
-            term = int((new_a[i[0]].get('x') + new_a[i[0] + 1].get('x')) / 2)
+    for i, in range(1, len(new_a)):
+        if new_a[i[0]].get('y') < b.get('y') < new_a[i[0] - 1].get('y'):
+            b['x'] = int((new_a[i[0]].get('x') + new_a[i[0] -1].get('x')) / 2)
+            print(new_a[i[0]], new_a[i[0] - 1])
+            term = int((new_a[i[0]].get('x') + new_a[i[0] - 1].get('x')) / 2)
             break
 
     new_a += [b]

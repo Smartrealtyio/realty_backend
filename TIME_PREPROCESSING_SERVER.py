dbd6bd3526c85c6f428f8725a53a1fb72380f60b
StefanPushkov <stefan.pushkov@icloud.com>
2020-01-16 19:16:32 +0300

    USe both: Yandex And Cian

--- TIME_PREPROCESSING_SERVER.py
+++ TIME_PREPROCESSING_SERVER.py
@@ -33,13 +33,13 @@ def main_preprocessing():
         flats = pd.read_csv(raw_data+ "flats.csv",
                                           names=['id', 'full_sq', 'kitchen_sq', 'life_sq', 'floor', 'is_apartment',
                                                  'building_id', 'created_at',
-                                                 'updated_at', 'offer_id', 'closed', 'rooms', 'image', 'resource_id'],
+                                                 'updated_at', 'closed', 'rooms', 'image', 'resource_id'],
                                           usecols=["id", "full_sq",
                                                    "kitchen_sq",
                                                    "life_sq",
                                                    "floor", "is_apartment",
                                                    "building_id",
-                                                   "closed", 'rooms', 'offer_id', 'resource_id'
+                                                   "closed", 'rooms', 'resource_id'
                                                    ],
                                           true_values="t", false_values="f", header=0)
         buildings = pd.read_csv(raw_data+ "buildings.csv",

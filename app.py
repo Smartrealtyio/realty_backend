4c22e0bf8dd2c42e381714f679d0eb77b5c0d6ab
StefanPushkov <stefan.pushkov@icloud.com>
2020-01-21 22:26:41 +0300

    add 'new point'

--- app.py
+++ app.py
@@ -592,10 +592,10 @@ def map():
     new_point_x = int(new_point.get('x'))
     new_point_y = int(new_point.get('y'))
     print("x from last item: ", new_point_x, flush=True)
-    new_point['x'] = new_point_x+2
-    new_point['y'] = new_point_y+new_point_y*0.02
+
+   
     print('new x: ', new_point, flush=True)
-    new_a = new_a+[new_point]
+    new_a.append({"x": new_point_x+2, 'y': new_point_y+new_point_y*0.02})
     print("Finally new_a: ", new_a, flush=True)
     # Check if enough data for plotting
 

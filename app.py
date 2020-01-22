34bf4e7290ebbbb79ddfbcbadfb56c1c50cf147f
StefanPushkov <stefan.pushkov@icloud.com>
2020-01-22 17:33:41 +0300

    capture 500

--- app.py
+++ app.py
@@ -230,7 +230,7 @@ def map():
 
     # Create subsample of flats with same cluster label value (from same "geographical" district)
     df_for_current_label = data[data.clusters == current_label[0]]
-    if len(df_for_current_label) < 1:
+    if df_for_current_label.shape < 1:
         return jsonify({'Price': 0, 'Duration': 0, 'PLot': 0, 'FlatsTerm': 0, "OOPS": 1})
 
     # Drop Price and Term Outliers using Z-Score

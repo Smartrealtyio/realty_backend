6a4744f6b484cffb894c1c66fdbf08f8b957cf8d
StefanPushkov <stefan.pushkov@icloud.com>
2020-03-12 15:14:15 +0300

    Remove 'Sub_cluster'

--- app.py
+++ app.py
@@ -348,7 +348,7 @@ def map():
     df_for_current_label = df_for_current_label[df_for_current_label.price.between(df_for_current_label.term.quantile(.1), df_for_current_label.price.quantile(.9))]
 
 
-    print("Shape: ", df_for_current_label.shape)
+    print("Shape: ", df_for_current_label.shape, flush=True)
     # Calculate price for each flat in SubSample based on price prediction models we have trained
     if secondary == 0:
         df_for_current_label['pred_price'] = df_for_current_label[

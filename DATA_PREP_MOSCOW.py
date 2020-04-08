273e23c40b255ef868f3cc5582be9c6de51ab395
StefanPushkov <stefan.pushkov@icloud.com>
2020-04-08 22:36:11 +0300

    TEST: new data preprocessing pipeline SPB, MOSCOW

--- DATA_PREP_MOSCOW.py
+++ DATA_PREP_MOSCOW.py
@@ -72,8 +72,6 @@ class MainPreprocessing():
         # Replace all missed values in CLOSED with 'False'
         flats.closed = flats.closed.fillna(False)
 
-        # Leave only VTORICHKA
-        # flats = flats[flats.flat_type == 'SECONDARY']
         flats = flats.rename(columns={"id": "flat_id"})
 
         buildings = pd.read_csv(raw_data + "buildings.csv",
@@ -228,24 +226,33 @@ class MainPreprocessing():
         # Get lenght of all df
         len_df = len(df)
 
+
         def add_fictive_rows(data: pd.DataFrame(), len_df: int):
-
+            rooms = 1
             for i in range(len(data), len(data)+8):
-                data.loc[i, 'rooms'] = i%len_df
+                data.loc[i, 'rooms'] = rooms
+                rooms += 1
 
             updated_len_df = len(data)
+
+            mm = 1
             for i in range(updated_len_df+1, updated_len_df+13):
-                data.loc[i, 'mm_announce'] = i%updated_len_df
+                data.loc[i, 'mm_announce'] = mm
+                mm+=1
 
             updated_len_df = len(data)
 
+            yy = 18
             for i in range(updated_len_df + 18, updated_len_df + now.year - 2000 + 1):
-                data.loc[i, 'yyyy_announce'] = i % updated_len_df
+                data.loc[i, 'yyyy_announce'] = yy
+                yy += 1
 
             updated_len_df = len(data)
-            for i in range(updated_len_df, updated_len_df+130):
-                data.loc[i, 'clusters'] = i%updated_len_df
 
+            cl = 0
+            for i in range(updated_len_df, updated_len_df+130):
+                data.loc[i, 'clusters'] = cl
+                cl += 1
             return data
 
         df = add_fictive_rows(data=df, len_df=len_df)

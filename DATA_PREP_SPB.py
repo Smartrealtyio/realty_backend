d2a4a824b740d4b055d90fdbe1b79f2400699bb2
StefanPushkov <stefan.pushkov@icloud.com>
2020-04-09 01:15:00 +0300

    TEST: new data preprocessing pipeline SPB, MOSCOW

--- DATA_PREP_SPB.py
+++ DATA_PREP_SPB.py
@@ -394,30 +394,7 @@ class MainPreprocessing():
 
         data = data[data.closed == True]
         print(list(data.dtypes), flush=True)
-        print(data[['full_sq', 'kitchen_sq', 'life_sq', 'is_apartment',
-                    'renovation', 'has_elevator',
-                    'time_to_metro', 'floor_first', 'floor_last',
-                    'is_rented', 'rent_quarter',
-                    'rent_year', 'to_center', 'was_opened', 'mm_announce__1']].head(10), flush=True)
-        print(data[['mm_announce__2', 'mm_announce__3', 'mm_announce__4',
-                 'mm_announce__5', 'mm_announce__6', 'mm_announce__7', 'mm_announce__8', 'mm_announce__9',
-                 'mm_announce__10', 'mm_announce__11', 'mm_announce__12', 'rooms__0']].head(10), flush=True)
-        print(data[['rooms__1', 'rooms__2', 'rooms__3', 'rooms__4', 'rooms__5', 'rooms__6', 'yyyy_announce__18',
-                 'yyyy_announce__19', 'yyyy_announce__20',
-                 'cluster__0', 'cluster__1',
-                 'cluster__2', 'cluster__3', 'cluster__4', 'cluster__5', 'cluster__6', 'cluster__7', 'cluster__8']].head(10), flush=True)
-        print(data[['cluster__9', 'cluster__10', 'cluster__11',
-                 'cluster__11', 'cluster__12', 'cluster__13', 'cluster__14', 'cluster__15', 'cluster__16',
-                 'cluster__17', 'cluster__18', 'cluster__19',
-                 'cluster__20', 'cluster__21', 'cluster__22', 'cluster__23', 'cluster__24',]].head(10), flush=True)
-        print(data[['cluster__25', 'cluster__26', 'cluster__27', 'cluster__28', 'cluster__29', 'cluster__30',
-                 'cluster__31', 'cluster__32',
-                 'cluster__33', 'cluster__34', 'cluster__35', 'cluster__36', 'cluster__37', 'cluster__38',
-                 'cluster__39', 'cluster__40',]].head(10), flush=True)
-        print(data[['cluster__41', 'cluster__42', 'cluster__43', 'cluster__44', 'cluster__45', 'cluster__46',
-                 'cluster__47', 'cluster__48', 'cluster__49', 'cluster__50', 'cluster__51', 'cluster__52',
-                 'cluster__53', 'cluster__54', 'cluster__55',
-                 'cluster__56', 'cluster__57', 'cluster__58', 'cluster__59']].head(10), flush=True)
+
 
         data['pred_price'] = data[['full_sq', 'kitchen_sq', 'life_sq', 'is_apartment',
                  'renovation', 'has_elevator',
@@ -455,7 +432,7 @@ class MainPreprocessing():
                   row.yyyy_announce__18, row.yyyy_announce__19, row.yyyy_announce__20,
                   row.cluster__0, row.cluster__1, row.cluster__2, row.cluster__3, row.cluster__4, row.cluster__5,
                   row.cluster__6, row.cluster__7,
-                  row.cluster__8, row.cluster__9, row.cluster__10, row.cluster__11,
+                  row.cluster__8, row.cluster__9, row.cluster__10,
                   row.cluster__11, row.cluster__12, row.cluster__13, row.cluster__14, row.cluster__15, row.cluster__16,
                   row.cluster__17, row.cluster__18, row.cluster__19,
                   row.cluster__20, row.cluster__21, row.cluster__22, row.cluster__23, row.cluster__24,

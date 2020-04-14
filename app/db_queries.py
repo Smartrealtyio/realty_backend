02e911b3c5ef63c055f64b8353c93901d4daeb00
Egor14 <egorgrekhnev@mail.ru>
2020-04-14 14:28:35 +0300

    time_to_metro query fix

--- app/db_queries.py
+++ app/db_queries.py
@@ -9,12 +9,12 @@ def get_other_params(flats):
     cur = conn.cursor()
     for flat in flats:
         # print(flat.keys(), flush=True)
-        cur.execute("select metro_id, time_to_metro from time_metro_buildings where building_id=%s",
+        cur.execute("select metro_id, time_to_metro from time_metro_buildings where building_id=%s and transport_type='ON_FOOT';",
                     (flat['building_id'],))
         metros_info = cur.fetchall()
         flat['metros'] = []
         for metro in metros_info:
-            cur.execute("select name from metros where id=%s", (metro[0],))
+            cur.execute("select name from metros where id=%s;", (metro[0],))
             flat['metros'].append({'station': cur.fetchone()[0], 'time_to_metro': metro[1]})
 
         if flat['resource_id'] == 0:
@@ -22,7 +22,7 @@ def get_other_params(flats):
         else:
             flat['link'] = 'https://www.cian.ru/sale/flat/' + str(flat['offer_id'])
 
-        cur.execute("select address from buildings where id=%s",
+        cur.execute("select address from buildings where id=%s;",
                     (flat['building_id'],))
         flat['address'] = cur.fetchone()[0]
 

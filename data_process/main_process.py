e3bebea3d18ac780ea92cd9ffeda5f3cc8b5244a
StefanPushkov <stefan.pushkov@icloud.com>
2020-05-21 15:38:53 +0300

    delete unnceessary prints

--- data_process/main_process.py
+++ data_process/main_process.py
@@ -299,86 +299,8 @@ def map_estimation(longitude, rooms, latitude, full_sq, kitchen_sq, life_sq, ren
         answ = {'Price': price, 'Duration': 0, 'PLot': [{"x": 0, 'y': 0}], 'FlatsTerm': 0, "OOPS": 1}
         return answ
 
-    # Create SUB Classes KMeans clustering based on size of subsample
-    # n = int(sqrt(df_for_current_label.shape[0]))
-    # kmeans_sub = KMeans(n_clusters=n, random_state=42).fit(df_for_current_label[
-    #                                                            ['full_sq', 'life_sq', 'kitchen_sq', 'time_to_metro',
-    #                                                             'longitude', 'latitude',
-    #                                                             'renovation']])  # , 'nums_of_changing']])
-    #
-    # # Set new column equals to new SUBclusters values
-    # labels = kmeans_sub.labels_
-    # df_for_current_label['SUB_cluster'] = labels
-    #
-    # SUB_cluster = kmeans_sub.predict([[full_sq, life_sq, kitchen_sq, time_to_metro, longitude, latitude, renovation]])
-    # # print(df_for_current_label.SUB_cluster.unique(), flush=True)
-    #
-    # df_for_current_label = df_for_current_label[df_for_current_label.SUB_cluster == SUB_cluster[0]]
-    #
-    # if len(df_for_current_label) < 2:
-    #     df_for_current_label = data[data.clusters == current_cluster[0]]
-    #
-    # Create new feature: number of flats in each SUBcluster
-    # df_for_current_label['num_of_flats_in_SUB_cluster'] = df_for_current_label.groupby(['SUB_cluster'])["SUB_cluster"].transform("count")
-    #
-    # Drop Outliers using Z-Score / 15-85 quartiles
-    # price outliers removing
-    # df_for_current_label = df_for_current_label[
-    #     df_for_current_label.price.between(df_for_current_label.price.quantile(.15),
-    #                                        df_for_current_label.price.quantile(.85))]
-    # #  term outliers removing
-    # df_for_current_label = df_for_current_label[
-    #     df_for_current_label.term.between(df_for_current_label.term.quantile(.15),
-    #                                       df_for_current_label.term.quantile(.85))]
-    # # squares outliers removing
-    # df_for_current_label = df_for_current_label[
-    #     df_for_current_label.full_sq.between(df_for_current_label.full_sq.quantile(.15),
-    #                                          df_for_current_label.full_sq.quantile(.85))]
-    # df_for_current_label = df_for_current_label[
-    #     df_for_current_label.life_sq.between(df_for_current_label.life_sq.quantile(.15),
-    #                                          df_for_current_label.life_sq.quantile(.85))]
-    # df_for_current_label = df_for_current_label[
-    #     df_for_current_label.kitchen_sq.between(df_for_current_label.kitchen_sq.quantile(.15),
-    #                                             df_for_current_label.kitchen_sq.quantile(.85))]
-    #
-    # # Calculate price for each flat in SubSample based on price prediction models we have trained
-    # if secondary == 0:
-    #     df_for_current_label['pred_price'] = df_for_current_label[
-    #         ['life_sq', 'rooms', 'renovation', 'has_elevator', 'longitude', 'latitude', 'full_sq', 'kitchen_sq',
-    #          'time_to_metro', 'floor_last', 'floor_first', 'clusters', 'is_rented', 'rent_quarter', 'rent_year']].apply(
-    #         lambda row:
-    #         int(((np.expm1(rf.predict([[np.log1p(row.life_sq), row.rooms, row.renovation, row.has_elevator,
-    #                                     np.log1p(row.longitude), np.log1p(row.latitude), np.log1p(row.full_sq),
-    #                                     np.log1p(row.kitchen_sq), row.time_to_metro, row.floor_last, row.floor_first,
-    #                                     row.clusters, row.is_rented, row.rent_quarter, row.rent_year]]))) +
-    #              (np.expm1(lgbm.predict([[np.log1p(row.life_sq), row.rooms, row.renovation, row.has_elevator,
-    #                                       np.log1p(row.longitude), np.log1p(row.latitude), np.log1p(row.full_sq),
-    #                                       np.log1p(row.kitchen_sq), row.time_to_metro, row.floor_last, row.floor_first,
-    #                                       row.clusters, row.is_rented, row.rent_quarter, row.rent_year]]))))[0] / 2),
-    #         axis=1)
-    #     pass
-    #
-    # if secondary == 1:
-    #     df_for_current_label['pred_price'] = df_for_current_label[
-    #         ['life_sq', 'rooms', 'renovation', 'has_elevator', 'longitude', 'latitude', 'full_sq', 'kitchen_sq',
-    #          'time_to_metro', 'floor_last', 'floor_first', 'clusters']].apply(
-    #         lambda row:
-    #         int(((np.expm1(rf.predict([[np.log1p(row.life_sq), row.rooms, row.renovation, row.has_elevator,
-    #                                     np.log1p(row.longitude), np.log1p(row.latitude), np.log1p(row.full_sq),
-    #                                     np.log1p(row.kitchen_sq), row.time_to_metro, row.floor_last, row.floor_first,
-    #                                     row.clusters]]))) +
-    #              (np.expm1(lgbm.predict([[np.log1p(row.life_sq), row.rooms, row.renovation, row.has_elevator,
-    #                                       np.log1p(row.longitude), np.log1p(row.latitude), np.log1p(row.full_sq),
-    #                                       np.log1p(row.kitchen_sq), row.time_to_metro, row.floor_last, row.floor_first,
-    #                                       row.clusters]]))))[0] / 2), axis=1)
-    #     pass
-    # # Calculate the profitability for each flat knowing the price for which the flat was sold and the price that
-    # # our model predicted
-    # df_for_current_label['profit'] = df_for_current_label[['pred_price', 'price']].apply(
-    #     lambda row: ((row.pred_price / row.price)), axis=1)
-    # print(df_for_current_label[['profit', 'price', 'pred_price']].head(), flush=True)
 
-    # # Drop flats which sold more than 600 days
+    # Drop flats which sold more than 600 days
     df_for_current_label = df_for_current_label[df_for_current_label.term <= 600]
 
     # Check if still enough samples
@@ -444,9 +366,9 @@ def map_estimation(longitude, rooms, latitude, full_sq, kitchen_sq, life_sq, ren
                 profit = i / price_meter_sq
                 # Calculate term based on profit for each price
                 term_on_profit = np.expm1(reg.predict([[np.log1p(profit), np.log1p(i)]]))
-                print(
-                    "Predicted term is {0} based on {1} profit and price_meter_sq {2}: ".format(term_on_profit, profit,
-                                                                                                i), flush=True)
+                # print(
+                #     "Predicted term is {0} based on {1} profit and price_meter_sq {2}: ".format(term_on_profit, profit,
+                #                                                                                 i), flush=True)
                 list_of_terms.append(term_on_profit)
 
             return list_of_terms

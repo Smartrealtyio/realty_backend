b1ddebcde3968fdcd76bae3562dccc270c866a98
StefanPushkov <stefan.pushkov@icloud.com>
2020-05-27 13:20:53 +0300

    Delete repeated month for first graphic

--- data_process/main_process.py
+++ data_process/main_process.py
@@ -680,9 +680,8 @@ class Developers_API():
         for mm_announce in list_of_months:
 
             # Check if current month is January, change year + 1
-            if yyyy_announce >= sale_start_year:
-                counter = yyyy_announce - sale_start_year
-            if mm_announce == 1 and counter > 0:
+
+            if mm_announce == 1:
                 yyyy_announce += 1
 
 
